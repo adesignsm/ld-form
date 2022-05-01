4b10b1881e32dfac1907a2b0d8fe76e02ce5ca4a
Akash <adesigns>
2022-05-01 19:51:12 -0400

    added data validation

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -2,9 +2,10 @@ import React, { useEffect, useState } from "react";
 
 import {initializeApp} from "firebase/app";
 import {getAnalytics} from "firebase/analytics";
-import {getDatabase, ref, set} from "firebase/database";
+import {get, getDatabase, ref, set, child, onValue} from "firebase/database";
 
 import "../form.css";
+import { type } from "@testing-library/user-event/dist/type";
 
 const firebaseConfig = {
     apiKey: "AIzaSyAJDQNNmSpwplbJfN8vWTFP5gXbIpHNeOA",
@@ -29,6 +30,36 @@ const Form = () => {
         return;
     }, [emailData], [nameData], [ldNumberData]);
 
+    const readUserData = (email, ld_number, username) => {
+
+        console.log(email, ld_number, username);
+
+        const dbRef = ref(getDatabase());
+
+        get(child(dbRef, `users/`)).then((snapshot) => {
+            if (snapshot.exists()) {
+                const dataSnapshot = snapshot.val();
+
+                for (const data in dataSnapshot) {
+                    console.log("EMAIL: ", dataSnapshot[data].email);
+                    console.log("NUMBER: ", dataSnapshot[data].ld_number);
+                    console.log("NAME: ", dataSnapshot[data].username);
+
+                    if (dataSnapshot[data].email.indexOf(email) !== -1 || dataSnapshot[data].ld_number.indexOf(ld_number) !== -1) {
+                        console.log("exisiting data");
+                        alert("You already have an exisiting ld account");
+                    } else {
+                        writeUserData(username, email, ld_number);
+                    }
+                }
+            } else {
+                console.log("no data");
+            }
+        }).catch((error) => {
+            console.error(error);
+        })
+    }
+
     const writeUserData = (name, email, number) => {
         const db = getDatabase();
 
@@ -40,6 +71,7 @@ const Form = () => {
     }
 
     const handleSubmit = () => {
+
         if (nameData.length === 0 || emailData.length === 0 || ldNumberData === 0) {
             console.error("empty field");
         } else {
@@ -47,23 +79,21 @@ const Form = () => {
             document.getElementById("email-input").value = "";
             document.getElementById("ld-number-input").value = "";
 
-            writeUserData(nameData, emailData, ldNumberData);
+            readUserData(emailData, ldNumberData, nameData);
+            // writeUserData(nameData, emailData, ldNumberData);
         }
     }
 
     return (
         <React.Fragment>
-            <form id = "LD-form" onSubmit={() => {handleSubmit()}}>
+            <form id = "LD-form" 
+                onSubmit={() => {handleSubmit()}}>
                 <label htmlFor = "name"> Name </label>
                 <input required type = "text" id = "name-input" name = "name" onChange={(e) => setNameData(e.target.value)}/>
                 <br />
+
                 <label htmlFor = "ld-number"> Ld number </label>
-                <input required 
-                    placeholder="Any number 200 - 1000" 
-                    type="number" 
-                    // maxLength="4" 
-                    id="ld-number-input" 
-                    name="ld-number" 
+                <input required placeholder="Any number 200 - 1000" type="number" id="ld-number-input" name="ld-number" 
                     onKeyDown={(e) => {
                         if (e.target.value.length >= 4) {
                             console.log("too big");
@@ -73,9 +103,11 @@ const Form = () => {
                     onChange={(e) => setLdNumberData(e.target.value)}
                 />
                 <br />
+
                 <label htmlFor = "email"> Email </label>
                 <input required type = "email" id = "email-input" name = "email" onChange={(e) => setEmailData(e.target.value)}/>
                 <br />
+
                 <input type = "submit" id = "submit-button" onMouseDown={() => handleSubmit()} /> 
             </form>
         </React.Fragment>
